17b204b6b99d5476fbdfaa48ba77a7be522ebb0d
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-07-25 16:50:49 +0200

    [UPD] added function to generate 10 email trought an API

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            email: null
+            email: []
         }
     },
     methods: {
+        // funzione per generare 10 email
+        generateTenEmails() {
 
+            for (let i = 0; i < 10; i++) {
+                // API
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
+                    
+                    this.email.push(response.data.response)
+                });
+            }
+        }
     }
 }).mount('#app');
